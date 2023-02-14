8471e11bf0488c7e4e6625bec7931ad32056b748
coder <yonghueijean1115@gmail.com>
2023-02-14 15:51:43 +0800

    set restart game

--- Form1.cs
+++ Form1.cs
@@ -13,9 +13,11 @@ namespace Floppy_Bird_Windows_Form
     public partial class Form1 : Form
     {
 
-        int pipeSpeed = 8;
-        int gravity = 5;
-        int score = 0;
+        int pipeSpeed = 8;      // default pipe speed defined with an integer
+        int gravity = 5;        // default gravity speed defined with an integer
+        int score = 0;         // default score integer set to 0
+
+        bool gameOver = false;
         public Form1()
         {
             InitializeComponent();
@@ -23,31 +25,38 @@ namespace Floppy_Bird_Windows_Form
 
         private void gameTimerEvent(object sender, EventArgs e)
         {
-            flappyBird.Top += gravity;
-            pipeBottom.Left -= pipeSpeed;
-            pipeTop.Left -= pipeSpeed;
-            scoreText.Text = "Score: " + score;
+            flappyBird.Top += gravity;      // link the flappy bird picture box to the gravity, += means it will add the speed of gravity to the picture boxes top location so it will move down
+            pipeBottom.Left -= pipeSpeed;       // link the bottom pipes left position to the pipe speed integer, it will reduce the pipe speed value from the left position of the pipe picture box so it will move left with each tick
+            pipeTop.Left -= pipeSpeed;      // the same is happening with the top pipe, reduce the value of pipe speed integer from the left position of the pipe using the -= sign
+            scoreText.Text = "Score: " + score;     // show the current score on the score text label
 
-            if(pipeBottom.Left < -150)
+            if (pipeBottom.Left < -150)
             {
+                // if the bottom pipes location is -150 then we will reset it back to 800 and add 1 to the score
                 pipeBottom.Left = 800;
                 score++;
             }
             if(pipeTop.Left < -180)
             {
+                // if the top pipe location is -180 then we will reset the pipe back to the 950 and add 1 to the score
                 pipeTop.Left = 950;
                 score++;
             }
+
+            // the if statement below is checking if the pipe hit the ground, pipes or if the player has left the screen from the top
+            // the two pipe symbols stand for OR inside of an if statement so we can have multiple conditions inside of this if statement because its all going to do the same thing
 
             if (flappyBird.Bounds.IntersectsWith(pipeBottom.Bounds) || 
                 flappyBird.Bounds.IntersectsWith(pipeTop.Bounds) ||
                 flappyBird.Bounds.IntersectsWith(ground.Bounds)
                 )
             {
+                // if any of the conditions are met from above then we will run the end game function
                 endGame();
             }
 
-            if(score > 5)
+            // if score is greater then 5 then we will increase the pipe speed to 15
+            if (score > 5)
             {
                 pipeSpeed = 15;
             }
@@ -60,25 +69,50 @@ namespace Floppy_Bird_Windows_Form
 
         private void gamekeyisdown(object sender, KeyEventArgs e)
         {
-            if(e.KeyCode == Keys.Space)
+            // this is the game key is down event thats linked to the main form
+            if (e.KeyCode == Keys.Space)
             {
+                // if the space key is pressed then the gravity will be set to -5
                 gravity = -5;
             }
         }
 
         private void gamekeyisup(object sender, KeyEventArgs e)
         {
+            // this is the game key is up event thats linked to the main form
             if (e.KeyCode == Keys.Space)
             {
+                // if the space key is released then gravity is set back to 5
                 gravity = 5;
+            }
+            if(e.KeyCode == Keys.R && gameOver)
+            {
+                // run the restart function
+                RestartGame();
             }
         }
 
         private void endGame()
         {
-            gameTimer.Stop();
-            scoreText.Text += "\n\n\n                             Game Over!";
+            // this is the game end function, this function will when the bird touches the ground or the pipes
+            gameTimer.Stop();       // stop the main timer
+            scoreText.Text += "\n\n\n           Game Over!!! Press R to Retry";      // show the game over text on the score text, += is used to add the new string of text next to the score instead of overriding it
+
+            gameOver = true;
         }
 
+        private void RestartGame()
+        {
+            gameOver = false;
+
+            flappyBird.Location = new Point(119, 186);
+            pipeTop.Left = 800;
+            pipeBottom.Left = 1200;
+
+            score = 0;
+            pipeSpeed = 8;
+            scoreText.Text = "Score: 0";
+            gameTimer.Start();
+        }
     }
 }
